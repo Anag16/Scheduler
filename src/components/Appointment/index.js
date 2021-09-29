e21a3fd5c8c4cb02d68a94e03afc64c220ec260e
Ana Ruiz <ruizana393@gmail.com>
2021-09-29 00:03:46 -0400

    Empty Component

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import classnames from "classnames"
+import "components/Appointment/styles.scss";
+
+export default function Appointment(props) {
+  //  const buttonClass = classnames("button", {
+  //    "button--confirm": props.confirm,
+  //    "button--danger": props.danger
+  //  });
+ 
+   return (
+    <article className="appointment">Appointment</article>
+   );
+ }
+ 
